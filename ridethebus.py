148c71ab60ae165e5a4b581e8dd64d5a1ec20ae2
François Hardy <snaptraks@gmail.com>
2019-09-12 09:34:57 -0400

    Use HatBot's cards classes

--- ridethebus.py
+++ ridethebus.py
@@ -1,13 +1,13 @@
 """Simulates the 'Ride the Bus' Game"""
 
-import carddeck
+import cards
 
 def rtb_round1():
     """Round one of Ride the Bus: Black or Red"""
     global win_state
-    card = deck.cards.pop()
+    card = deck.pop_card()
     #   Assign a color based on suit
-    if card.suit == 'spades' or card.suit == 'clubs': 
+    if card.suit_name == 'Spades' or card.suit_name == 'Clubs':
         color = 'black'
     else:
         color = 'red'
@@ -19,41 +19,40 @@ def rtb_round1():
         if guess != 'black' and guess != 'red':
             print("That's not a valid entry. Please guess again.")
         elif guess == color:
-            card.show()
+            print(card)
             print("That's right! Moving on...")
             round_cards.append(card)
             win_state = True
             active = False
         elif guess != color:
-            card.show()
+            print(card)
             print("That's incorrect. Restarting...")
             active = False
 
 def rtb_round2():
     """Round two of Ride the Bus: Higher or Lower"""
     global win_state
-    card = deck.cards.pop()
-    print("Round 2:")
+    card = deck.pop_card()
     active = True
     #   Play round
     while active:
-        guess = input(f"Higher or lower than {round_cards[0].value}?\n\t")
+        guess = input(f"Higher or lower than {round_cards[0].rank}?\n\t")
         guess = guess.lower()
         if guess != 'lower' and guess != 'higher':
             print("That's not a valid entry. Please guess again.")
-        elif (guess == 'lower' and card.value < round_cards[0].value or 
-                guess == 'higher' and card.value > round_cards[0].value):
-            card.show()
+        elif (guess == 'lower' and card.rank < round_cards[0].rank or
+                guess == 'higher' and card.rank > round_cards[0].rank):
+            print(card)
             print("Correct! Moving on...")
             round_cards.append(card)
             active = False
-        elif card.value == round_cards[0].value:
-            card.show()
+        elif card.rank == round_cards[0].rank:
+            print(card)
             print("It's a tie! You lose. Restarting...")
             win_state = False
             active = False
         else:
-            card.show()
+            print(card)
             print("Incorrect! Restarting...")
             win_state = False
             active = False
@@ -61,35 +60,34 @@ def rtb_round2():
 def rtb_round3():
     """Round three of Ride the Bus: In Between or Outside"""
     global win_state
-    card = deck.cards.pop()
+    card = deck.pop_card()
     #   Build a list of values from rounds 1 and 2
-    c_values = []
+    c_ranks = []
     for c in round_cards:
-        c_values.append(c.value)
+        c_ranks.append(c.rank)
     #   Make sure list is low - high
-    c_values = sorted(c_values)
-    print("Round 3:")
+    c_ranks = sorted(c_ranks)
     #   Play round
     active = True
     while active:
-        guess = input(f"Inside or outside of {c_values[0]} and {c_values[1]}\n\t")
+        guess = input(f"Inside or outside of {c_ranks[0]} and {c_ranks[1]}\n\t")
         guess = guess.lower()
         if guess != 'inside' and guess != 'outside':
             print("That's not a valid entry. Please guess again.")
-        elif (guess == 'inside' and card.value > c_values[0] and 
-                card.value < c_values[1]):
-            card.show()
+        elif (guess == 'inside' and card.rank > c_ranks[0] and
+                card.rank < c_ranks[1]):
+            print(card)
             print("Correct! Moving on...")
             round_cards.append(card)
             active = False
-        elif (guess == 'outside' and card.value < c_values[0] or
-                card.value > c_values[1]):
-            card.show()
+        elif (guess == 'outside' and card.rank < c_ranks[0] or
+                card.rank > c_ranks[1]):
+            print(card)
             print("Correct! Moving on...")
             round_cards.append(card)
             active = False
         else:
-            card.show()
+            print(card)
             print("Incorrect! Restarting...")
             win_state = False
             active = False
@@ -97,8 +95,7 @@ def rtb_round3():
 def rtb_round4():
     """Round 4 of Ride the Bus: Suit"""
     global win_state
-    card = deck.cards.pop()
-    print("Round 4:")
+    card = deck.pop_card()
     #   Play round
     active = True
     while active:
@@ -106,19 +103,19 @@ def rtb_round4():
         guess = guess.lower()
         if guess not in ['spades', 'hearts', 'clubs', 'diamonds']:
             print("That's not a valid entry. Please guess again")
-        elif guess == card.suit:
-            card.show()
+        elif guess == card.suit_name.lower():
+            print(card)
             print("Correct!")
             round_cards.append(card)
             active = False
-        elif guess != card.suit:
-            card.show()
+        elif guess != card.suit_name.lower():
+            print(card)
             print("Incorrect! Restarting...")
             win_state = False
             active = False
 
 
-deck = carddeck.Deck()
+deck = cards.Deck()
 deck.shuffle()
 round_cards = []
 win_state = False
